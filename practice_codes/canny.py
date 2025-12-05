c720dd6e2efe67e1d64970f7413cb4c8863ae166
jhasu0202 <jhasujamisetty@gmail.com>
2025-12-05 19:06:13 +0530

    Add files via upload

---
+++ practice_codes/canny.py
@@ -0,0 +1,11 @@
+#CODE 10
+
+import cv2
+
+img = cv2.imread("/home/jhasu/Pictures/Infosys Practice/computer vision/images.jpeg", 0)
+
+edges = cv2.Canny(img, 100, 200)
+
+cv2.imshow("Edges", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
